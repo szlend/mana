cfec7da6cfc55b8a685a5dcb5485a5beea3ef283
Simon Žlender <simon.zlender@gmail.com>
2016-06-28 21:08:54 +0200

    Update to Phoenix 1.2

--- mix.exs
+++ mix.exs
@@ -30,10 +30,10 @@ defmodule Mana.Mixfile do
   #
   # Type `mix help deps` for examples and options.
   defp deps do
-    [{:phoenix, "~> 1.2.0-rc"},
-     {:phoenix_pubsub, "~> 1.0.0-rc"},
+    [{:phoenix, "~> 1.2.0"},
+     {:phoenix_pubsub, "~> 1.0.0"},
      {:phoenix_ecto, "~> 3.0"},
-     {:phoenix_html, "~> 2.5"},
+     {:phoenix_html, "~> 2.6"},
      {:phoenix_live_reload, "~> 1.0", only: :dev},
      {:postgrex, ">= 0.0.0"},
      {:gettext, "~> 0.11"},
