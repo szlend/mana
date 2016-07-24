07eb65c6faa1c227d249c68850b00326d80da197
Simon Žlender <simon.zlender@gmail.com>
2016-07-24 21:58:12 +0200

    Configure SSL

--- config/prod.exs
+++ config/prod.exs
@@ -13,9 +13,11 @@ use Mix.Config
 # which you typically run after static files are built.
 config :mana, Mana.Endpoint,
   http: [port: {:system, "PORT"}],
-  url: [host: "mana.my.to", port: 80],
+  url: [scheme: "https", host: "mana-game.herokuapp.com", port: 443],
+  force_ssl: [rewrite_on: [:x_forwarded_proto]],
   cache_static_manifest: "priv/static/manifest.json",
-  secret_key_base: System.get_env("SECRET_KEY_BASE")
+  secret_key_base: System.get_env("SECRET_KEY_BASE"),
+  ssl: true
 
 config :mana, Mana.Repo,
   adapter: Ecto.Adapters.Postgres,
