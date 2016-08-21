888aa833ecaf87572ccef77843618948f1266d01
Simon Žlender <simon.zlender@gmail.com>
2016-08-21 19:36:01 +0200

    Protect against invalid data

--- web/channels/game_channel.ex
+++ web/channels/game_channel.ex
@@ -15,19 +15,22 @@ defmodule Mana.GameChannel do
     end
   end
 
-  def handle_in("moves", %{"x" => x, "y" => y, "w" => w, "h" => h}, socket) do
+  def handle_in("moves", %{"x" => x, "y" => y, "w" => w, "h" => h}, socket)
+  when is_integer(x) and is_integer(y) and is_integer(w) and is_integer(h) do
     name = socket.assigns.game
     {:ok, moves} = GameInstance.moves(name, {x, y}, {w, h})
     {:reply, {:ok, %{moves: moves}}, socket}
   end
 
-  def handle_in("mines", %{"x" => x, "y" => y, "w" => w, "h" => h}, socket) do
+  def handle_in("mines", %{"x" => x, "y" => y, "w" => w, "h" => h}, socket)
+  when is_integer(x) and is_integer(y) and is_integer(w) and is_integer(h) do
     name = socket.assigns.game
     {:ok, mines} = GameInstance.mines(name, {x, y}, {w, h})
     {:reply, {:ok, %{mines: mines}}, socket}
   end
 
-  def handle_in("reveal", %{"x" => x, "y" => y}, socket) do
+  def handle_in("reveal", %{"x" => x, "y" => y}, socket)
+  when is_integer(x) and is_integer(y) do
     name = socket.assigns.game
     user_id = socket.assigns.id
     {:ok, moves} = GameInstance.reveal(name, user_id, {x, y})
